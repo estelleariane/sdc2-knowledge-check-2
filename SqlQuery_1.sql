d9433f3d0624236afa69437c94c809da226c3b7a
estelleariane <90357738+estelleariane@users.noreply.github.com>
2022-10-28 07:16:11 -0400

    Add files via upload

---
+++ SqlQuery_1.sql
@@ -0,0 +1,13 @@
+﻿SELECT a.customerName
+From Customers as a
+Join Orders as b on 
+a.CustomerID=b.CustomerID
+where b.OrderID=10310
+
+
+
+SELECT a.Address
+from Suppliers as a
+join Products as b on 
+a.SupplierID=b.SupplierID
+where b.ProductID=40 
